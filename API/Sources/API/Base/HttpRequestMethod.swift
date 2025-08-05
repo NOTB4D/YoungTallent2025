8ae9ccc4f11ec7a78d7654031fd377d926efc9d5
Eser Kucuker <eser.kucuker@mobven.com>
2025-08-05 13:00:42 +0300

    Add HttpRequestMethod enum for HTTP methods
    Introduces a public enum HttpRequestMethod representing common HTTP request methods (GET, PUT, POST, DELETE) for use throughout the API module.

---
+++ API/Sources/API/Base/HttpRequestMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HttpRequestMethod.swift
+//  API
+//
+//  Created by Eser Kucuker on 5.08.2025.
+//
+
+public enum HttpRequestMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
