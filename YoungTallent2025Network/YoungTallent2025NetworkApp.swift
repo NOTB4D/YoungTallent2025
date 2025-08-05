787c538c9af2b310191114a8a78afea3b927de91
Eser Kucuker <eser.kucuker@mobven.com>
2025-08-05 11:10:04 +0300

    first commit

---
+++ YoungTallent2025Network/YoungTallent2025NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoungTallent2025NetworkApp.swift
+//  YoungTallent2025Network
+//
+//  Created by Eser Kucuker on 5.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct YoungTallent2025NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YoungTallent2025NetworkTests/YoungTallent2025NetworkTests.swift
@@ -0,0 +1,16 @@
+//
+//  YoungTallent2025NetworkTests.swift
+//  YoungTallent2025NetworkTests
+//
+//  Created by Eser Kucuker on 5.08.2025.
+//
+
+import Testing
+
+struct YoungTallent2025NetworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
