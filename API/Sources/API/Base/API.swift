24aa6e53939e1d697ae83ea3d83a05d34c60a754
Eser Kucuker <eser.kucuker@mobven.com>
2025-08-05 13:00:25 +0300

    Add API utility for constructing backend URLs
    Introduces a public API enum with a static method to generate backend URLs using a base URL from the app's Info.plist. Adds error handling for missing or invalid URLs.

--- API/Sources/API/Base/API.swift
+++ API/Sources/API/Base/API.swift
@@ -1,2 +1,21 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  API.swift
+//  API
+//
+//  Created by Eser Kucuker on 5.08.2025.
+//
+
+import Foundation
+
+public enum API {
+    static func getAPIURL(_ trail: String) -> URL {
+        guard let baseURL = Bundle.main.infoForKey("BACKEND_URL") else {
+            fatalError("Backend Url Okunamadı")
+        }
+        
+        guard let url = URL(string: "\(baseURL)/\(trail)") else {
+            fatalError("Url Oluşturulamadı")
+        }
+        return url
+    }
+}
